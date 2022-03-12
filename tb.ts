8e492f3f47fdf12d04f44c5a68fec81f817c7f49
Tim Buntel <tim@buntel.com>
2022-03-12 16:15:41 -0500

    Update tb.ts
    Hello world

--- tb.ts
+++ tb.ts
@@ -1,2 +1,9 @@
-console.log('hello den');
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
+console.log("Listening on http://localhost:8000");
+serve((_req) => {
+  return new Response("Hello Deno!", {
+    headers: { "content-type": "text/plain" },
+  });
+});
 	
